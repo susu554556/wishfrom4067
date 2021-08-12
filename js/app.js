3bf41460e862615bf41f00cf5e909cba99a7b5ff
unknown <suthinn80@gmail.com>
2021-08-12 21:38:57 +0630

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+const audio = document.getElementById('audio');
+const body = document.querySelector('body');
+const home = document.getElementById('home-page');
+
+window.addEventListener('mousemove' ,(e)=>{
+    audio.play();
+});
